d6c9718f02a2cb56b564bfacb5174189c60620d6
Brianna Gonzales <borgeltgonzales@unm.edu>
2021-02-07 23:05:27 -0700

    Updating Code in Javascript Fixes #2

--- scripts/salescalc.js
+++ scripts/salescalc.js
@@ -1,9 +1,17 @@
 addEventListener("load", main)
 
+
+//These two will load in the listeners to the main account 
+//and will help them get to calculating sales
 function main() {
     btCalcSales.addEventListener("click", calculateSales)
 }
 
+
+/**
+ * This is the function of the code that will actually do the sales calculations
+ *  which will include the price, tax, and totals with taxed amounts
+ */
 function calculateSales() {
     Price=parseFloat(txPrice.value)
     TaxRate=parseFloat(txTaxRate.value)/100
